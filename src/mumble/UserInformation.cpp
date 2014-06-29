b4feeac75536d7a325094a6f37df9ca504ab5a16
Zuko <zuczeq@gmail.com>
2014-06-29 17:10:15 +0200

    Little change in Domain code

--- src/mumble/UserInformation.cpp
+++ src/mumble/UserInformation.cpp
@@ -164,9 +164,9 @@ void UserInformation::update(const MumbleProto::UserStats &msg) {
 		HostAddress ha(msg.address());
 		qlAddress->setText(ha.toString());
 
-        QHostInfo info = QHostInfo::fromName(ha.toString());
-        QString nameAddress =  info.hostName();
-        qlDomainName->setText(nameAddress);
+		QHostInfo hi = QHostInfo::fromName(ha.toString());
+		QString domainName = hi.hostName();
+		qlDomainName->setText(domainName);
 	}
 	if (msg.has_version()) {
 		showcon = true;
